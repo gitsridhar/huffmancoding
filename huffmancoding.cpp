c25e0e3afa6456a39bc5dcb9e911a96c4be768f1
Sridhar Venkat <svenkat@macbook-pro-4.raleigh.ibm.com>
2019-06-12 10:35:43 -0400

    Initial commit

---
+++ huffmancoding.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_TREE_HEIGHT 100
+
+class MinHeapNode {
+    public:
+        char data;
+        unsigned freq;
+
+        MinHeapNode *left, *right;
+};
+
+class MinHeap {
+    public:
+        unsigned size;
+        unsigned capacity;
+
+        MinHeapNode **array;
+};
+
+MinHeapNode *newNode(char data, unsigned freq) {
+    MinHeapNode *temp = new MinHeapNode();
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+
+    return temp;
+}
+
+MinHeap *createMinHeap(unsigned capacity) {
+    MinHeap *minHeap = new MinHeap();
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+
+    minHeap->array = new MinHeapNode*[capacity];
+    for(int i=0; i<capacity; ++i) {
+        minHeap->array[i] = new MinHeapNode();
+    }
+
+    return minHeap;
+}
+
+void swapMinHeapNode(MinHeapNode **a, MinHeapNode **b) {
+    MinHeapNode *t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void minHeapify(MinHeap *minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq) {
+        smallest = left;
+    }
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq) {
+        smallest = right;
+    }
+
+    if (smallest != idx) {
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+int isSizeOne(MinHeap *minHeap) {
+    return (minHeap->size == 1);
+}
+
+MinHeapNode *extractMin(MinHeap *minHeap) {
+    MinHeapNode *temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+
+    return temp;
+}
+
+void insertMinHeap(MinHeap *minHeap, MinHeapNode *minHeapNode) {
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+
+    while(i && minHeapNode->freq < minHeap->array[(i-1) / 2]->freq) {
+        minHeap->array[i] = minHeap->array[(i-1)/2];
+        i = (i-1)/2;
+    }
+
+    minHeap->array[i] = minHeapNode;
+}
+
+void buildMinHeap(MinHeap *minHeap) {
+    int n = minHeap->size - 1;
+    int i;
+
+    for(i=(n-1)/2; i>=0; --i) {
+        minHeapify(minHeap, i);
+    }
+}
+
+void print(int arr[], int n) {
+    for(int i=0; i<n; ++i) {
+        cout << arr[i];
+    }
+    cout << endl;
+}
+
+int isLeaf(MinHeapNode *root) {
+    return !(root->left) && !(root->right);
+}
+
+MinHeap *createAndBuildMinHeap(char data[], int freq[], int size) {
+    MinHeap *minHeap = createMinHeap(size);
+
+    for(int i=0; i<size; ++i) {
+        minHeap->array[i] = newNode(data[i], freq[i]);
+    }
+
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+
+    return minHeap;
+}
+
+MinHeapNode *buildHuffmanTree(char data[], int freq[], int size) {
+    MinHeapNode *left, *right, *top; 
+
+    MinHeap *minHeap = createAndBuildMinHeap(data, freq, size);
+
+    while(!isSizeOne(minHeap)) {
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+
+        insertMinHeap(minHeap, top);
+    }
+
+    return extractMin(minHeap);
+}
+
+void printCodes(MinHeapNode *root, int arr[], int top) {
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top+1);
+    }
+
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top+1);
+    }
+
+    if (isLeaf(root)) {
+        cout << root->data << ": ";
+        print(arr, top);
+    }
+}
+
+void HuffmanCodes(char data[], int freq[], int size) {
+    MinHeapNode *root = buildHuffmanTree(data, freq, size);
+
+    int arr[MAX_TREE_HEIGHT], top = 0;
+
+    printCodes(root, arr, top);
+}
+
+int main() {
+    char arr[] = {'o', 'a', 'b', 'c', 'd', 'e', 'f'};
+    int freq[] = {99,5,9,12,13,16,45 };
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    HuffmanCodes(arr, freq, size);
+
+    return 0;
+}
